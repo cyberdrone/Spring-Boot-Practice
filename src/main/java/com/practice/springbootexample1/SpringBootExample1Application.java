0e31b03bf374abdddd5ba78c962bd6aa22e26b87
Mr. Sniper <Mr. Sniper@Snipers-Outpost>
2020-11-12 19:18:00 +0530

    Draft

---
+++ src/main/java/com/practice/springbootexample1/SpringBootExample1Application.java
@@ -0,0 +1,14 @@
+package com.practice.springbootexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExample1Application.class, args);
+		System.out.println("Spring boot example is running.");
+	}
+
+}
